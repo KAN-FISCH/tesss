350b574c06363d1f15c75dc68da654ab1e0c4e60
KAN-FISCH <shieldteamid@gmail.com>
2025-09-12 01:14:07 +0700

    Update shieldguiee.lua

--- shieldguiee.lua
+++ shieldguiee.lua
@@ -62,7 +62,7 @@ local function OpenClose()
     BorderSizePixel = 1,
     Position = UDim2.new(0.1021, 0, 0.0743, 0),
     Size = UDim2.new(0, 59, 0, 49),
-    Image = "rbxassetid://85779221265543",
+    Image = "rbxassetid://125992172976297",
     Visible = false
   }, ScreenGui)
 
