59c9d47954d035a641a214ea681cc3b3840e0b7b
KAN-FISCH <shieldteamid@gmail.com>
2026-01-18 06:47:41 +0700

    Update and rename allscript.lua to allscript1.lua

--- allscript1.lua
+++ allscript1.lua
@@ -1,11 +1,17 @@
-local placeScripts = {
+
+local placeScriptsPremium = {
+    [131716211654599] = "https://api.shieldteam.asia/api/raw/fisch/premium",
+    [16732694052] = "https://api.shieldteam.asia/api/raw/fisch/premium",
+}
+
+local placeScriptsFree = {
     [86710642002144] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/Talamau.lua",
     [129009554587176] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/TheForge/tes",
     [76558904092080] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/TheForge/tes",
     [131884594917121] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/TheForge/tes",
     [2693023319] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/Antartika.lua",
-    [131716211654599] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/fish.lua",
-    [16732694052] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/fish.lua",
+    [131716211654599] = "https://api.shieldteam.asia/api/raw/fisch/free",
+    [16732694052] = "https://api.shieldteam.asia/api/raw/fisch/free",
     [95777993643016] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/Arunika.lua",
     [102234703920418] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/Daun.lua",
     [89100225746834] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/Tanditek.lua",
@@ -16,8 +22,16 @@ local placeScripts = {
     [103754275310547] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/huntyZomby.lua",
     [128473079243102] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/arunika2.lua",
     [14963184269] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/Sumbing.lua",
-    [121864768012064] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/fishit",
+    [121864768012064] = "https://raw.githubusercontent.com/KAN-FISCH/tesss/refs/heads/main/gunung/fishit"
 }
+
+local key = _G.Key or getgenv().Key
+local placeScripts = placeScriptsFree
+
+if key and tostring(key) ~= "" then
+    placeScripts = placeScriptsPremium
+end
+
 local currentPlaceId = game.PlaceId
 if placeScripts[currentPlaceId] then
     local scriptUrl = placeScripts[currentPlaceId]
